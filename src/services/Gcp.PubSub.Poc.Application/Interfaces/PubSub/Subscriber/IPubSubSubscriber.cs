93964aa5c51758aeece0d11ebef5184f5c85beb6
Kubing Lin <edkt973@gmail.com>
2025-08-26 15:41:01 +0800

    chore: 調整註解

--- src/services/Gcp.PubSub.Poc.Application/Interfaces/PubSub/Subscriber/IPubSubSubscriber.cs
+++ src/services/Gcp.PubSub.Poc.Application/Interfaces/PubSub/Subscriber/IPubSubSubscriber.cs
@@ -2,6 +2,17 @@ namespace Gcp.PubSub.Poc.Application.Interfaces.PubSub.Subscriber
 {
     public interface IPubSubSubscriber : IAsyncDisposable
     {
+        /// <summary>
+        /// 啟動背景訂閱工作
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="handleMessageAsync">
+        /// 1. 若工作太長，建議把消息丟到背景 job/queue 處理，再 ack 掉 Pub/Sub 訊息
+        /// 2. 冪等處理
+        /// 3. 調整 AckDeadline
+        /// </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<IPubSubSubscriberHandle> StartAsync(
             PubSubTaskConfig config,
             Func<PubSubPayload, CancellationToken, Task> handleMessageAsync,
